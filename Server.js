4590320566f77af9624e28511f2aed1a1898e122
vedant nandi <v.nandi14@gmail.com>
2021-07-19 20:37:20 +0530

    cheking

--- Server.js
+++ Server.js
@@ -89,7 +89,8 @@ app.post("/register", (req, res) => {
           })
           .then((user) => res.json(user[0]));
       });
-  }).catch((err) => res.status(400).json("user present"));
+  });
+  //.catch((err) => res.status(400).json("user present"));
 });
 
 // prfile
